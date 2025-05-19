4739955954f2584babcf92ea810f46b77baebc35
nahco314 <nahco3_ta@yahoo.co.jp>
2025-05-19 22:01:25 +0900

    fix: fix and cleanup

--- crates/tombi-wasm-compat/src/url.rs
+++ crates/tombi-wasm-compat/src/url.rs
@@ -4,7 +4,7 @@ pub fn url_from_file_path<P: AsRef<std::path::Path>>(path: P) -> Result<url::Url
 }
 
 #[cfg(feature = "wasm")]
-pub fn url_from_file_path<P: AsRef<std::path::Path>>(path: P) -> Result<url::Url, ()> {
+pub fn url_from_file_path<P: AsRef<std::path::Path>>(_path: P) -> Result<url::Url, ()> {
     Err(())
 }
 
@@ -14,6 +14,6 @@ pub fn url_to_file_path(url: &url::Url) -> Result<std::path::PathBuf, ()> {
 }
 
 #[cfg(feature = "wasm")]
-pub fn url_to_file_path(url: &url::Url) -> Result<std::path::PathBuf, ()> {
+pub fn url_to_file_path(_url: &url::Url) -> Result<std::path::PathBuf, ()> {
     Err(())
 }
