e5622f08b669900f8c36dbf9abd3afee07467003
ya7010 <ya7010@outlook.com>
2025-06-17 11:41:36 +0900

    feat: add "code-action" to completion labels test

--- crates/tombi-lsp/tests/test_completion_labels.rs
+++ crates/tombi-lsp/tests/test_completion_labels.rs
@@ -319,6 +319,7 @@ mod completion_labels {
                 "#,
                 Schema(tombi_schema_path()),
             ) -> Ok([
+                "code-action",
                 "completion",
                 "diagnostics",
                 "document-link",
