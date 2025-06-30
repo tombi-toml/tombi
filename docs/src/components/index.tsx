8c9378cf8463b9e321f3441124bab29935cd9d84
ya7010 <ya7010@outlook.com>
2025-06-30 23:56:28 +0900

    refactor: update component imports to Highlight2
    Replaced imports from Highlight to Highlight2 in various documentation files to reflect the new component structure.

--- docs/src/components/index.tsx
+++ docs/src/components/index.tsx
@@ -1,5 +1,5 @@
 // Export all components that should be available for MDX files
 export { CodeBlock } from "./CodeBlock";
-export { Note, Tip } from "./Highlight";
+export { Note, Tip } from "./Highlight2";
 export { LinkCard } from "./LinkCard";
 export { InstallationMethodGrid } from "./InstallationMethodGrid";
