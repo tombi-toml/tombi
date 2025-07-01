efe6d4ad551676ad6a21cfae6a18e3a0e468f097
nahco314 <nahco3_ta@yahoo.co.jp>
2025-07-02 03:53:27 +0900

    revert http_client implementation

--- crates/tombi-schema-store/src/http_client.rs
+++ crates/tombi-schema-store/src/http_client.rs
@@ -1,16 +1,19 @@
 mod error;
 
-#[cfg(feature = "reqwest01")]
+#[cfg(all(feature = "wasm", feature = "surf"))]
+compile_error!("Cannot enable both `wasm` and `surf` features");
+
+#[cfg(not(any(feature = "wasm", feature = "surf")))]
 mod reqwest_client;
-#[cfg(feature = "reqwest01")]
+#[cfg(not(any(feature = "wasm", feature = "surf")))]
 pub use reqwest_client::HttpClient;
 
-#[cfg(feature = "gloo-net06")]
+#[cfg(feature = "wasm")]
 mod gloo_net_client;
-#[cfg(feature = "gloo-net06")]
+#[cfg(feature = "wasm")]
 pub use gloo_net_client::HttpClient;
 
-#[cfg(feature = "surf2")]
+#[cfg(feature = "surf")]
 mod surf_client;
-#[cfg(feature = "surf2")]
+#[cfg(feature = "surf")]
 pub use surf_client::HttpClient;
