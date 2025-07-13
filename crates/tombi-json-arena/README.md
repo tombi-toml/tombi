7b370a1ad0cb55b55673ef671198c0d575907919
ya7010 <ya7010@outlook.com>
2025-07-13 14:17:05 +0900

    remove: tombi-json-arena

--- crates/tombi-json-arena/README.md
+++
@@ -1,3 +0,0 @@
-This is a just idea for optimization of the JSON parsing in Tombi.
-
-It's not use yet, but it could be used in the future to speed up the parsing of JSON Schema files.
--- crates/tombi-json-arena/src/error.rs
+++
@@ -1,9 +0,0 @@
-#[derive(Debug, thiserror::Error)]
-pub enum Error {
-    #[error("Lexer error: {0:?}")]
-    Lexer(tombi_json_lexer::Error),
-    #[error("Parser error: {0}")]
-    Parser(crate::parser::Error),
-    #[error("No value found")]
-    NoValue,
-}
