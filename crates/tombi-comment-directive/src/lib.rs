667f140a9ad00764b3c1590f8350a331d42b5f06
ya7010 <ya7010@outlook.com>
2025-08-10 17:23:03 +0900

    refactor: add try_get_root_comment_directive.

--- crates/tombi-comment-directive/src/lib.rs
+++ crates/tombi-comment-directive/src/lib.rs
@@ -4,21 +4,37 @@ mod root;
 pub use error::Error;
 pub use root::RootCommentDirective;
 use tombi_ast::AstNode;
+use tombi_diagnostic::SetDiagnostics;
 use tombi_document::IntoDocument;
 use tombi_document_tree::IntoDocumentTreeAndErrors;
 use tombi_toml_version::TomlVersion;
 
 #[cfg(feature = "serde")]
 pub fn get_root_comment_directive(root: &tombi_ast::Root) -> Option<RootCommentDirective> {
+    try_get_root_comment_directive(root).ok().flatten()
+}
+
+#[cfg(feature = "serde")]
+pub fn try_get_root_comment_directive(
+    root: &tombi_ast::Root,
+) -> Result<Option<RootCommentDirective>, Vec<tombi_diagnostic::Diagnostic>> {
     use serde::Deserialize;
 
+    let mut total_diagnostics = Vec::new();
     if let Some(tombi_directives) = root.tombi_directives() {
         const COMMENT_DIRECTIVE_TOML_VERSION: TomlVersion = TomlVersion::V1_0_0;
 
-        for (tombi_directive, _) in tombi_directives {
+        for (tombi_directive, tombi_directive_range) in tombi_directives {
             let parsed = tombi_parser::parse(&tombi_directive, COMMENT_DIRECTIVE_TOML_VERSION);
             // Check if there are any parsing errors
             if !parsed.errors.is_empty() {
+                let mut diagnostics = Vec::new();
+                for error in parsed.errors {
+                    error.set_diagnostics(&mut diagnostics);
+                }
+                total_diagnostics.extend(diagnostics.into_iter().map(|diagnostic| {
+                    into_directive_diagnostic(&diagnostic, tombi_directive_range)
+                }));
                 continue;
             }
             let root =
@@ -30,13 +46,42 @@ pub fn get_root_comment_directive(root: &tombi_ast::Root) -> Option<RootCommentD
 
             // Check for errors during document tree construction
             if !errors.is_empty() {
-                continue;
+                let mut diagnostics = Vec::new();
+                for error in errors {
+                    error.set_diagnostics(&mut diagnostics);
+                }
+                total_diagnostics.extend(diagnostics.into_iter().map(|diagnostic| {
+                    into_directive_diagnostic(&diagnostic, tombi_directive_range)
+                }));
             }
-            return RootCommentDirective::deserialize(
+
+            if let Ok(directive) = RootCommentDirective::deserialize(
                 &document_tree.into_document(COMMENT_DIRECTIVE_TOML_VERSION),
-            )
-            .ok();
+            ) {
+                return Ok(Some(directive));
+            }
         }
     }
-    None
+
+    if !total_diagnostics.is_empty() {
+        return Err(total_diagnostics);
+    } else {
+        Ok(None)
+    }
+}
+
+fn into_directive_diagnostic(
+    diagnostic: &tombi_diagnostic::Diagnostic,
+    tombi_directive_range: tombi_text::Range,
+) -> tombi_diagnostic::Diagnostic {
+    tombi_diagnostic::Diagnostic::new_warning(
+        diagnostic.message(),
+        diagnostic.code(),
+        tombi_text::Range::new(
+            tombi_directive_range.start
+                + tombi_text::RelativePosition::from(diagnostic.range().start),
+            tombi_directive_range.start
+                + tombi_text::RelativePosition::from(diagnostic.range().end),
+        ),
+    )
 }
