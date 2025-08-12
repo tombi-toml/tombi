d51c2a1263bedadb3121c7313fea7d9251286fb6
InSync <insyncwithfoo@gmail.com>
2025-08-12 10:54:15 +0700

    feat(intellij): Display Tombi icon in language server widget

--- editors/intellij/src/main/kotlin/tombi/server/TombiServerSupportProvider.kt
+++ editors/intellij/src/main/kotlin/tombi/server/TombiServerSupportProvider.kt
@@ -2,8 +2,12 @@ package tombi.server
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.platform.lsp.api.LspServer
 import com.intellij.platform.lsp.api.LspServerSupportProvider
 import com.intellij.platform.lsp.api.LspServerSupportProvider.LspServerStarter
+import com.intellij.platform.lsp.api.lsWidget.LspServerWidgetItem
+import tombi.Icons
+import tombi.configurations.TombiConfigurable
 import tombi.configurations.tombiConfigurations
 
 
@@ -16,6 +20,9 @@ import tombi.configurations.tombiConfigurations
  * @see TombiServerDescriptor
  */
 internal class TombiServerSupportProvider : LspServerSupportProvider {
+    
+    override fun createLspServerWidgetItem(lspServer: LspServer, currentFile: VirtualFile?) =
+        LspServerWidgetItem(lspServer, currentFile, Icons._16, TombiConfigurable::class.java)
     
     override fun fileOpened(project: Project, file: VirtualFile, serverStarter: LspServerStarter) {
         if (file.isTOMLFile) {
