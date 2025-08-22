ca636bf8c95fb3b7253ffdc239ea57f81e31056b
ya7010 <ya7010@outlook.com>
2025-08-23 08:16:10 +0900

    wip: add struct.

---
+++ crates/tombi-comment-directive/src/value/array.rs
@@ -0,0 +1,26 @@
+use tombi_severity_level::SeverityLevelDefaultError;
+
+#[derive(Debug, Default, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+#[serde(deny_unknown_fields)]
+#[serde(rename_all = "kebab-case")]
+#[cfg_attr(feature = "jsonschema", derive(schemars::JsonSchema))]
+#[cfg_attr(feature = "jsonschema", schemars(extend("$id" = "tombi://json.tombi.dev/array-tombi-directive.json")))]
+pub struct ArrayTombiCommentDirective {
+    /// Controls the severity level for type mismatch errors
+    pub type_mismatch: Option<SeverityLevelDefaultError>,
+
+    /// Controls the severity level for const value errors
+    pub const_value: Option<SeverityLevelDefaultError>,
+
+    /// Controls the severity level for enumerate value errors
+    pub enumerate: Option<SeverityLevelDefaultError>,
+
+    /// Controls the severity level for max values errors
+    pub max_values: Option<SeverityLevelDefaultError>,
+
+    /// Controls the severity level for min values errors
+    pub min_values: Option<SeverityLevelDefaultError>,
+
+    /// Controls the severity level for unique values errors
+    pub unique_values: Option<SeverityLevelDefaultError>,
+}
