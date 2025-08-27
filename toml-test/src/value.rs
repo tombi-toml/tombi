93baf343da82bc94bd4e9db079257cf4d0bccd71
ya7010 <ya7010@outlook.com>
2025-08-28 04:39:28 +0900

    remove ValueType.

--- toml-test/src/value.rs
+++ toml-test/src/value.rs
@@ -1,22 +1,16 @@
 use tombi_toml_version::TomlVersion;
 
 #[derive(Debug, serde::Serialize)]
-#[serde(rename_all = "kebab-case")]
-pub enum Type {
-    Bool,
-    Integer,
-    Float,
-    String,
-    Datetime,
-    DatetimeLocal,
-    DateLocal,
-    TimeLocal,
-}
-
-#[derive(Debug, serde::Serialize)]
 #[serde(untagged)]
 pub enum Value {
-    Literal { r#type: Type, value: String },
+    Bool(bool),
+    Integer(i64),
+    Float(f64),
+    String(String),
+    OffsetDatetime(String),
+    LocalDatetime(String),
+    LocalDate(String),
+    LocalTime(String),
     Array(Vec<Value>),
     Table(indexmap::IndexMap<String, Value>),
 }
@@ -28,38 +22,22 @@ pub trait IntoValue {
 impl IntoValue for tombi_document_tree::Value {
     fn into_value(self, toml_version: TomlVersion) -> Value {
         match self {
-            tombi_document_tree::Value::Boolean(value) => Value::Literal {
-                r#type: Type::Bool,
-                value: value.value().to_string(),
-            },
-            tombi_document_tree::Value::Integer(value) => Value::Literal {
-                r#type: Type::Integer,
-                value: value.value().to_string(),
-            },
-            tombi_document_tree::Value::Float(value) => Value::Literal {
-                r#type: Type::Float,
-                value: value.value().to_string(),
-            },
-            tombi_document_tree::Value::String(value) => Value::Literal {
-                r#type: Type::String,
-                value: value.into_value(),
-            },
-            tombi_document_tree::Value::OffsetDateTime(value) => Value::Literal {
-                r#type: Type::Datetime,
-                value: value.value().to_string(),
-            },
-            tombi_document_tree::Value::LocalDateTime(value) => Value::Literal {
-                r#type: Type::DatetimeLocal,
-                value: value.value().to_string(),
-            },
-            tombi_document_tree::Value::LocalDate(value) => Value::Literal {
-                r#type: Type::DateLocal,
-                value: value.value().to_string(),
-            },
-            tombi_document_tree::Value::LocalTime(value) => Value::Literal {
-                r#type: Type::TimeLocal,
-                value: value.value().to_string(),
-            },
+            tombi_document_tree::Value::Boolean(value) => Value::Bool(value.value()),
+            tombi_document_tree::Value::Integer(value) => Value::Integer(value.value()),
+            tombi_document_tree::Value::Float(value) => Value::Float(value.value()),
+            tombi_document_tree::Value::String(value) => Value::String(value.into_value()),
+            tombi_document_tree::Value::OffsetDateTime(value) => {
+                Value::OffsetDatetime(value.value().to_string())
+            }
+            tombi_document_tree::Value::LocalDateTime(value) => {
+                Value::LocalDatetime(value.value().to_string())
+            }
+            tombi_document_tree::Value::LocalDate(value) => {
+                Value::LocalDate(value.value().to_string())
+            }
+            tombi_document_tree::Value::LocalTime(value) => {
+                Value::LocalTime(value.value().to_string())
+            }
             tombi_document_tree::Value::Array(array) => Value::Array(
                 array
                     .into_iter()
