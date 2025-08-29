25ee6aba7863830eca9ef5efeac6d6abfbe70fe9
ya7010 <ya7010@outlook.com>
2025-08-29 16:18:15 +0900

    fix: title.

--- crates/tombi-lsp/src/comment_directive.rs
+++ crates/tombi-lsp/src/comment_directive.rs
@@ -1,14 +1,14 @@
 use tombi_ast::{SchemaDocumentCommentDirective, TombiDocumentCommentDirective};
 
-pub const DOCUMENT_SCHEMA_DIRECTIVE_TITLE: &str = "DocumentSchema Directive";
+pub const DOCUMENT_SCHEMA_DIRECTIVE_TITLE: &str = "Schema Document Directive";
 pub const DOCUMENT_SCHEMA_DIRECTIVE_DESCRIPTION: &str =
     "Specify the Schema URL/Path for the document.";
 
-pub const DOCUMENT_TOMBI_DIRECTIVE_TITLE: &str = "Document Tombi Directive";
+pub const DOCUMENT_TOMBI_DIRECTIVE_TITLE: &str = "Tombi Document Directive";
 pub const DOCUMENT_TOMBI_DIRECTIVE_DESCRIPTION: &str =
     "Directives that apply only to this document.";
 
-pub const VALUE_TOMBI_DIRECTIVE_TITLE: &str = "Value Tombi Directive";
+pub const VALUE_TOMBI_DIRECTIVE_TITLE: &str = "Tombi Value Directive";
 pub const VALUE_TOMBI_DIRECTIVE_DESCRIPTION: &str = "Directives that apply only to this value.";
 
 pub fn get_schema_document_comment_directive(
