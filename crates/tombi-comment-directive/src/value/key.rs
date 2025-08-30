72e41dbbdaebdb948dbec0ddb0c67e68a98c9088
ya7010 <ya7010@outlook.com>
2025-08-31 03:49:13 +0900

    chore: change description.

--- crates/tombi-comment-directive/src/value/key.rs
+++ crates/tombi-comment-directive/src/value/key.rs
@@ -28,20 +28,24 @@ pub struct KeyRules {
     /// # VALID BUT DISCOURAGED
     /// "" = true
     /// ```
+    ///
     pub key_empty: Option<SeverityLevelDefaultWarn>,
 
     /// # Key required.
     ///
     /// Check if the key is required in this Table.
+    ///
     pub key_required: Option<SeverityLevelDefaultError>,
 
     /// # Key not allowed.
     ///
     /// Check if the key is not defined in this Table.
+    ///
     pub key_not_allowed: Option<SeverityLevelDefaultError>,
 
     /// # Key pattern.
     ///
     /// Check if the key matches the pattern in the Schema.
+    ///
     pub key_pattern: Option<SeverityLevelDefaultError>,
 }
