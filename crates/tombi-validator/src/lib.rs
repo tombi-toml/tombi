a14b0b06b2003e42badbdbd63e11abc58bd6f7b6
ya7010 <ya7010@outlook.com>
2025-09-01 20:54:21 +0900

    chore: remove unuse code.

--- crates/tombi-validator/src/lib.rs
+++ crates/tombi-validator/src/lib.rs
@@ -2,8 +2,6 @@ pub mod comment_directive;
 mod diagnostic;
 mod validate;
 
-pub use comment_directive::{
-    get_tombi_value_comment_directive, get_tombi_value_comment_directive_and_diagnostics,
-};
+pub use comment_directive::get_tombi_value_comment_directive_and_diagnostics;
 pub use diagnostic::{Diagnostic, DiagnosticKind};
 pub use validate::{validate, Validate};
