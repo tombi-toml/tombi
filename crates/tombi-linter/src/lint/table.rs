955e621bd9c080542120f1b3cd2aef0ed5a8a155
ya7010 <ya7010@outlook.com>
2025-09-09 01:50:32 +0900

    test: add test cases.

--- crates/tombi-linter/src/lint/table.rs
+++ crates/tombi-linter/src/lint/table.rs
@@ -37,5 +37,66 @@ mod tests {
                 actual: 0,
             }]);
         }
+
+        test_lint! {
+            #[test]
+            fn test_table_min_keys_with_leading_comment_directive(
+                r#"
+                # tombi: lint.rules.table-min-keys.disabled = true
+                [table]
+                "#,
+                type_test_schema_path(),
+            ) -> Ok(_);
+        }
+
+        test_lint! {
+            #[test]
+            fn test_table_unknown_min_keys(
+                r#"
+                #:tombi schema.strict = false
+
+                [table.""]
+                "#,
+                type_test_schema_path(),
+            ) -> Err([crate::DiagnosticKind::KeyEmpty]);
+        }
+
+        test_lint! {
+            #[test]
+            fn test_table_unknown_min_keys_with_leading_comment_directive(
+                r#"
+                #:tombi schema.strict = false
+
+                # tombi: lint.rules.key-empty.disabled = true
+                [table.""]
+                "#,
+                type_test_schema_path(),
+            ) -> Ok(_);
+        }
+
+        test_lint! {
+            #[test]
+            fn test_table_unknown_min_keys_with_trailing_comment_directive(
+                r#"
+                #:tombi schema.strict = false
+
+                [table.""] # tombi: lint.rules.key-empty.disabled = true
+                "#,
+                type_test_schema_path(),
+            ) -> Ok(_);
+        }
+
+        test_lint! {
+            #[test]
+            fn test_table_unknown_min_keys_with_dangling_comment_directive(
+                r#"
+                #:tombi schema.strict = false
+
+                [table.""]
+                # tombi: lint.rules.key-empty.disabled = true
+                "#,
+                type_test_schema_path(),
+            ) -> Err([crate::DiagnosticKind::KeyEmpty]);
+        }
     }
 }
