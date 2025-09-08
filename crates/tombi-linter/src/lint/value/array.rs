8bcecb34bb4ec7630ad76b4cc5efacb968edc0d3
ya7010 <ya7010@outlook.com>
2025-09-09 02:52:49 +0900

    chore: test cases.

--- crates/tombi-linter/src/lint/value/array.rs
+++ crates/tombi-linter/src/lint/value/array.rs
@@ -140,5 +140,37 @@ mod tests {
                 tombi_validator::DiagnosticKind::KeyNotAllowed {key: "key-empty".to_string()}
             ]);
         }
+
+        test_lint! {
+            #[test]
+            fn test_nexted_array(
+                r#"
+                array = [[]]
+                "#,
+                type_test_schema_path(),
+            ) -> Err([
+                tombi_validator::DiagnosticKind::ArrayMinValues {
+                    min_values: 2,
+                    actual: 1,
+                }
+            ]);
+        }
+
+        test_lint! {
+            #[test]
+            fn test_nexted_array_min_values_with_array_leading_and_key_dangling_comment_directive(
+                r#"
+                # tombi: lint.rules.array-min-values.disabled = true
+                array = [
+                    [
+                    # tombi: lint.rules.key-empty.disabled = true
+                    ]
+                ]
+                "#,
+                type_test_schema_path(),
+            ) -> Err([
+                tombi_validator::DiagnosticKind::KeyNotAllowed {key: "key-empty".to_string()}
+            ]);
+        }
     }
 }
