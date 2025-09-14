3310cfba07153471ff4c5488d3056b5bf6ec7f43
ya7010 <ya7010@outlook.com>
2025-09-15 00:08:40 +0900

    refactor: array edit.

--- crates/tombi-ast-editor/src/edit/array.rs
+++ crates/tombi-ast-editor/src/edit/array.rs
@@ -4,8 +4,10 @@ use itertools::Itertools;
 use tombi_ast::AstNode;
 use tombi_comment_directive::value::{ArrayCommonLintRules, ArrayFormatRules};
 use tombi_comment_directive_serde::get_comment_directive_content;
+use tombi_document_tree::IntoDocumentTreeAndErrors;
 use tombi_future::{BoxFuture, Boxable};
-use tombi_schema_store::ValueSchema;
+use tombi_schema_store::{AllOfSchema, AnyOfSchema, OneOfSchema, ValueSchema};
+use tombi_validator::Validate;
 
 use crate::rule::{array_comma_trailing_comment, array_values_order};
 
@@ -24,36 +26,76 @@ impl crate::Edit for tombi_ast::Array {
 
             let mut use_item_schema = false;
             if let Some(current_schema) = current_schema {
-                if let ValueSchema::Array(array_schema) = current_schema.value_schema.as_ref() {
-                    if let Some(item_schema) = &array_schema.items {
-                        if let Ok(Some(current_schema)) = item_schema
-                            .write()
-                            .await
-                            .resolve(
-                                Cow::Borrowed(&current_schema.schema_uri),
-                                Cow::Borrowed(&current_schema.definitions),
-                                schema_context.store,
-                            )
-                            .await
-                            .inspect_err(|err| tracing::warn!("{err}"))
-                        {
-                            use_item_schema = true;
-                            for (value, comma) in self.values_with_comma() {
-                                changes
-                                    .extend(array_comma_trailing_comment(&value, comma.as_ref()));
-                                changes.extend(
-                                    value
+                match current_schema.value_schema.as_ref() {
+                    ValueSchema::Array(array_schema) => {
+                        if let Some(item_schema) = &array_schema.items {
+                            if let Ok(Some(current_schema)) = item_schema
+                                .write()
+                                .await
+                                .resolve(
+                                    Cow::Borrowed(&current_schema.schema_uri),
+                                    Cow::Borrowed(&current_schema.definitions),
+                                    schema_context.store,
+                                )
+                                .await
+                                .inspect_err(|err| tracing::warn!("{err}"))
+                            {
+                                use_item_schema = true;
+                                for (value, comma) in self.values_with_comma() {
+                                    changes.extend(array_comma_trailing_comment(
+                                        &value,
+                                        comma.as_ref(),
+                                    ));
+                                    changes.extend(
+                                        value
+                                            .edit(
+                                                &[],
+                                                source_path,
+                                                Some(&current_schema),
+                                                schema_context,
+                                            )
+                                            .await,
+                                    );
+                                }
+                            }
+                        }
+                    }
+                    ValueSchema::AllOf(AllOfSchema { schemas, .. })
+                    | ValueSchema::AnyOf(AnyOfSchema { schemas, .. })
+                    | ValueSchema::OneOf(OneOfSchema { schemas, .. }) => {
+                        let value = &self
+                            .clone()
+                            .into_document_tree_and_errors(schema_context.toml_version)
+                            .tree;
+
+                        for referable_schema in schemas.write().await.iter_mut() {
+                            if let Ok(Some(current_schema)) = referable_schema
+                                .resolve(
+                                    current_schema.schema_uri.clone(),
+                                    current_schema.definitions.clone(),
+                                    schema_context.store,
+                                )
+                                .await
+                                .inspect_err(|err| tracing::warn!("{err}"))
+                            {
+                                if value
+                                    .validate(&[], Some(&current_schema), schema_context)
+                                    .await
+                                    .is_ok()
+                                {
+                                    return self
                                         .edit(
                                             &[],
                                             source_path,
                                             Some(&current_schema),
                                             schema_context,
                                         )
-                                        .await,
-                                );
+                                        .await;
+                                }
                             }
                         }
                     }
+                    _ => {}
                 }
             }
             if !use_item_schema {
