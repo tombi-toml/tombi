47fdbda62a222840f4bd87f0da2fae1e59fe1d77
ya7010 <ya7010@outlook.com>
2025-09-18 19:35:19 +0900

    fix: semantic_tokens bug.

--- crates/tombi-lsp/src/semantic_tokens/table.rs
+++ crates/tombi-lsp/src/semantic_tokens/table.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use tombi_ast::AstNode;
 
 use super::{AppendSemanticTokens, SemanticTokensBuilder, TokenType};
@@ -26,19 +27,29 @@ impl AppendSemanticTokens for tombi_ast::Table {
             comment.append_semantic_tokens(builder);
         }
 
-        for comments in self.key_values_begin_dangling_comments() {
-            for comment in comments {
-                comment.append_semantic_tokens(builder);
+        let key_values = self.key_values().collect_vec();
+
+        if key_values.is_empty() {
+            for comments in self.key_values_dangling_comments() {
+                for comment in comments {
+                    comment.append_semantic_tokens(builder);
+                }
             }
-        }
+        } else {
+            for comments in self.key_values_begin_dangling_comments() {
+                for comment in comments {
+                    comment.append_semantic_tokens(builder);
+                }
+            }
 
-        for key_value in self.key_values() {
-            key_value.append_semantic_tokens(builder);
-        }
+            for key_value in key_values {
+                key_value.append_semantic_tokens(builder);
+            }
 
-        for comments in self.key_values_end_dangling_comments() {
-            for comment in comments {
-                comment.append_semantic_tokens(builder);
+            for comments in self.key_values_end_dangling_comments() {
+                for comment in comments {
+                    comment.append_semantic_tokens(builder);
+                }
             }
         }
     }
