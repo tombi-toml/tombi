f11fb97d3b2aa245e8f0746c0dedce8fb264d7de
ya7010 <ya7010@outlook.com>
2025-09-29 07:50:08 +0900

    fix: add eq.

--- crates/tombi-lsp/src/semantic_tokens/key_value.rs
+++ crates/tombi-lsp/src/semantic_tokens/key_value.rs
@@ -1,5 +1,7 @@
 use tombi_ast::AstNode;
 
+use crate::semantic_tokens::TokenType;
+
 use super::{AppendSemanticTokens, SemanticTokensBuilder};
 
 impl AppendSemanticTokens for tombi_ast::KeyValue {
@@ -10,6 +12,10 @@ impl AppendSemanticTokens for tombi_ast::KeyValue {
 
         if let Some(key) = self.keys() {
             key.append_semantic_tokens(builder)
+        }
+
+        if let Some(token) = self.eq() {
+            builder.add_token(TokenType::OPERATOR, token.into())
         }
 
         if let Some(value) = self.value() {
