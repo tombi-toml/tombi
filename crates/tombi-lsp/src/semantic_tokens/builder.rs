326a1d0690cdc58e4fea9b70bb34239aad1ea7c2
ya7010 <ya7010@outlook.com>
2025-09-29 08:17:45 +0900

    wip: multibyte text.

--- crates/tombi-lsp/src/semantic_tokens/builder.rs
+++ crates/tombi-lsp/src/semantic_tokens/builder.rs
@@ -1,6 +1,7 @@
 use tombi_ast::AstToken;
-use tombi_text::{FromLsp, IntoLsp};
+use tombi_text::{FromLsp, IntoLsp, WideEncoding};
 use tower_lsp::lsp_types::SemanticToken;
+use unicode_segmentation::UnicodeSegmentation;
 
 use super::token_type::TokenType;
 
@@ -25,15 +26,18 @@ impl<'a> SemanticTokensBuilder<'a> {
     }
 
     pub fn add_token(&mut self, token_type: TokenType, elem: tombi_syntax::SyntaxElement) {
-        let range = elem.range();
+        let range: tombi_text::Range = elem.range();
 
-        let relative = relative_range(range, self.last_range, self.line_index);
+        let delta = tower_lsp::lsp_types::Position::from_lsp(
+            range.start - self.last_range.start,
+            self.line_index,
+        );
 
         #[allow(clippy::cast_possible_truncation)]
         self.tokens.push(SemanticToken {
-            delta_line: relative.start.line as u32,
-            delta_start: relative.start.character as u32,
-            length: elem.span().len(),
+            delta_line: delta.line as u32,
+            delta_start: delta.character as u32,
+            length: token_length(range, self.line_index),
             token_type: token_type as u32,
             token_modifiers_bitset: 0,
         });
@@ -48,7 +52,10 @@ impl<'a> SemanticTokensBuilder<'a> {
     ) {
         let comment_range = comment.as_ref().syntax().range();
 
-        let relative = relative_range(comment_range, self.last_range, self.line_index);
+        let delta = tower_lsp::lsp_types::Position::from_lsp(
+            comment_range.start - self.last_range.start,
+            self.line_index,
+        );
         let directive_range =
             tower_lsp::lsp_types::Range::from_lsp(directive_range, self.line_index);
         self.last_range = comment_range;
@@ -57,8 +64,8 @@ impl<'a> SemanticTokensBuilder<'a> {
         let directive_len = directive_range.end.character - directive_range.start.character;
 
         self.tokens.push(SemanticToken {
-            delta_line: relative.start.line as u32,
-            delta_start: relative.start.character as u32,
+            delta_line: delta.line as u32,
+            delta_start: delta.character as u32,
             length: prefix_len,
             token_type: TokenType::COMMENT as u32,
             token_modifiers_bitset: 0,
@@ -86,22 +93,41 @@ impl<'a> SemanticTokensBuilder<'a> {
     }
 }
 
-fn relative_range(
-    from: tombi_text::Range,
-    to: tombi_text::Range,
-    line_index: &tombi_text::LineIndex,
-) -> tower_lsp::lsp_types::Range {
-    let line_diff = from.end.line - from.start.line;
-    let start = from.start - to.start;
+fn token_length(range: tombi_text::Range, line_index: &tombi_text::LineIndex) -> u32 {
+    let wide_encoding = line_index.wide_encoding;
 
-    let end = if line_diff == 0 {
-        tombi_text::Position::new(
-            start.line,
-            start.column + from.end.column - from.start.column,
-        )
+    if range.start.line == range.end.line {
+        let Some(line_text) = line_index.line_text(range.start.line) else {
+            tracing::error!("line_text is None. line: {}", range.start.line);
+            return 0;
+        };
+        let line_text_graphemes = line_text.graphemes(true);
+        line_text_graphemes
+            .skip(range.start.column as usize)
+            .take((range.end.column - range.start.column) as usize)
+            .fold(0, |acc, char| acc + wide_encoding.measure(char))
     } else {
-        tombi_text::Position::new(start.line + line_diff, from.end.column)
-    };
-
-    tombi_text::Range::new((start.line, start.column).into(), end).into_lsp(line_index)
+        (range.start.line..=range.end.line).fold(0, |acc, line| {
+            acc + line_index
+                .line_text(line)
+                .map(|line_text| {
+                    let line_text_graphemes = line_text.graphemes(true);
+                    let skip_count = if line == range.start.line {
+                        range.start.column as usize
+                    } else {
+                        0
+                    };
+                    let take_count = if line == range.end.line {
+                        (range.end.column - range.start.column) as usize
+                    } else {
+                        line_text_graphemes.size_hint().1.unwrap()
+                    };
+                    line_text_graphemes
+                        .skip(skip_count)
+                        .take(take_count)
+                        .fold(0, |acc, char| acc + wide_encoding.measure(char))
+                })
+                .unwrap_or_default()
+        })
+    }
 }
