fa69487f5cf7386f0cca8d9a9a4191dd93ab7254
ya7010 <ya7010@outlook.com>
2025-10-22 20:00:40 +0900

    feat: update dependencies in Cargo.toml and Cargo.lock; add TextEdit struct for completion feature

--- crates/tombi-parser/src/parse/inline_table.rs
+++ crates/tombi-parser/src/parse/inline_table.rs
@@ -7,6 +7,7 @@ use crate::{
         trailing_comment, Parse,
     },
     parser::Parser,
+    token_set::TS_LINE_END,
     ErrorKind::*,
 };
 
@@ -27,6 +28,8 @@ impl Parse for tombi_ast::InlineTable {
         let mut key_value_lines = 0;
         let mut last_comma_range = None;
         loop {
+            while p.eat_ts(TS_LINE_END) {}
+
             let n = peek_leading_comments(p);
             if p.nth_at(n, EOF) || p.nth_at(n, T!['}']) {
                 break;
