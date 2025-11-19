de61ab33d2206ec8fabcd38545f1faba87d26e01
ya7010 <ya7010@outlook.com>
2025-11-19 22:34:22 +0900

    feat: lint warning reulst.

--- crates/tombi-lsp/src/hover/one_of.rs
+++ crates/tombi-lsp/src/hover/one_of.rs
@@ -127,9 +127,7 @@ where
                             valid_hover_value_contents.insert(hover_value_content.clone());
                         }
                         Err(tombi_validator::Error { diagnostics, .. })
-                            if diagnostics
-                                .iter()
-                                .all(|error| error.level() == tombi_diagnostic::Level::WARNING) =>
+                            if diagnostics.iter().all(|diagnostic| diagnostic.is_warning()) =>
                         {
                             valid_hover_value_contents.insert(hover_value_content.clone());
                         }
