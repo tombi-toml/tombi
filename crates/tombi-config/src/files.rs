5926b6d7df983d26808cecd98110d8e18213a3db
ya7010 <ya7010@outlook.com>
2025-11-26 14:10:19 +0900

    update: default excludes.

--- crates/tombi-config/src/files.rs
+++ crates/tombi-config/src/files.rs
@@ -19,11 +19,23 @@ pub struct FilesOptions {
     /// The file match pattern to exclude from formatting and linting.
     /// Supports glob pattern.
     #[cfg_attr(feature = "jsonschema", schemars(length(min = 1)))]
+    #[cfg_attr(feature = "serde", serde(default = "default_exclude_patterns"))]
     pub exclude: Option<Vec<String>>,
 }
 
 fn default_include_patterns() -> Option<Vec<String>> {
     Some(vec!["**/*.toml".to_string()])
+}
+
+fn default_exclude_patterns() -> Option<Vec<String>> {
+    Some(vec![
+        "Cargo.lock".to_string(),
+        "Gopkg.lock".to_string(),
+        "Pipfile".to_string(),
+        "pdm.lock".to_string(),
+        "poetry.lock".to_string(),
+        "uv.lock".to_string(),
+    ])
 }
 
 impl Default for FilesOptions {
