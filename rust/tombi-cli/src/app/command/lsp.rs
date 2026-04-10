8b59ca5950c4df4de6864e65e962a3dfa55472b2
ya7010 <ya7010@outlook.com>
2026-04-11 01:05:39 +0900

    Avoid hanging on LSP shutdown

--- rust/tombi-cli/src/app/command/lsp.rs
+++ rust/tombi-cli/src/app/command/lsp.rs
@@ -1,4 +1,5 @@
 use crate::app::CommonArgs;
+use std::time::Duration;
 
 /// Run TOML Language Server.
 #[derive(Debug, clap::Args)]
@@ -9,14 +10,16 @@ pub struct Args {
 
 pub fn run(args: impl Into<Args>) -> Result<(), crate::Error> {
     let args: Args = args.into();
-    tokio::runtime::Builder::new_current_thread()
+    let runtime = tokio::runtime::Builder::new_current_thread()
         .enable_all()
-        .build()?
-        .block_on(tombi_lsp::serve(
-            tombi_lsp::Args {},
-            args.common.offline,
-            args.common.no_cache,
-        ));
+        .build()?;
+
+    runtime.block_on(tombi_lsp::serve(
+        tombi_lsp::Args {},
+        args.common.offline,
+        args.common.no_cache,
+    ));
+    runtime.shutdown_timeout(Duration::from_secs(1));
 
     Ok(())
 }
