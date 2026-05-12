9f5d77bbd8e9fb7014b2fba52e4923e7062e3f08
ya7010 <ya7010@outlook.com>
2026-05-12 22:04:40 +0900

    chore: add icon.

--- editors/vscode/src/command/show-actions.ts
+++ editors/vscode/src/command/show-actions.ts
@@ -8,18 +8,22 @@ export async function showActions(commandPrefix: string): Promise<void> {
   const items: ActionItem[] = [
     {
       label: "Select Schema",
+      iconPath: new vscode.ThemeIcon("json"),
       command: `${commandPrefix}.selectSchema`,
     },
     {
       label: "Refresh Cache",
+      iconPath: new vscode.ThemeIcon("trash"),
       command: `${commandPrefix}.refreshCache`,
     },
     {
       label: "Restart Language Server",
+      iconPath: new vscode.ThemeIcon("refresh"),
       command: `${commandPrefix}.restartLanguageServer`,
     },
     {
       label: "Open Language Server Logs",
+      iconPath: new vscode.ThemeIcon("output"),
       command: `${commandPrefix}.openServerLogs`,
     },
   ];
