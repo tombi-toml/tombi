555ace5ef4f89acc38ec9e1a41cce5a08557d944
ya7010 <47286750+ya7010@users.noreply.github.com>
2026-08-02 17:19:49 +0900

    fix(docs): preserve interactions across navigation (#2051)

--- docs/src/components/CodeBlock.tsx
+++ docs/src/components/CodeBlock.tsx
@@ -1,3 +1,9 @@
+import Prism from "prismjs";
+import "prismjs/components/prism-toml";
+import "prismjs/components/prism-bash";
+import "prismjs/components/prism-json";
+import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-powershell";
 import type { ParentComponent } from "solid-js";
 import { CopyButton } from "./CopyButton";
 
@@ -6,16 +12,40 @@ interface CodeBlockProps {
   language?: string;
 }
 
+function escapeHtml(value: string) {
+  return value.replace(
+    /[&<>"']/g,
+    (character) =>
+      ({
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        '"': "&quot;",
+        "'": "&#39;",
+      })[character] || character,
+  );
+}
+
 export const CodeBlock: ParentComponent<CodeBlockProps> = (props) => {
+  const language = () => props.language || "text";
+  const highlightedCode = () => {
+    const grammar = Prism.languages[language()];
+    return grammar
+      ? Prism.highlight(props.code, grammar, language())
+      : escapeHtml(props.code);
+  };
+
   return (
     <div class="code-block-wrapper relative max-w-full overflow-hidden my-4">
       <pre
-        class={`language-${props.language || "text"} overflow-x-auto max-w-full pr-20`}
+        class={`language-${language()} overflow-x-auto max-w-full pr-20`}
         tabindex="-1"
       >
-        <code class={`language-${props.language || "text"}`} tabindex="-1">
-          {`${props.code}`}
-        </code>
+        <code
+          class={`language-${language()}`}
+          tabindex="-1"
+          innerHTML={highlightedCode()}
+        />
       </pre>
       <div class="copy-button language-text">
         <CopyButton text={props.code} />
